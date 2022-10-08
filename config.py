47a9adf5e421ebc95fa77dc91700fcb3b7608576
Ketan Bhailikar <81752891+MonotonicCode@users.noreply.github.com>
2022-10-08 11:00:49 +0530

    Added the Config file
    This file contains all the global variables that will be shared among all the other python modules in this project

---
+++ config.py
@@ -0,0 +1,14 @@
+# This file contains the global variables that
+# have to be shared among all the other python modules.
+
+# Global variables
+page_width = 1700       # the page width and page height are set to the A4 sheet's ratio
+page_height = 2404
+
+current_page_number = 1
+
+# Image variables set to None ( Initially )
+current_character_img = None
+current_word_img = None
+current_line_img = None
+current_page_img = None
