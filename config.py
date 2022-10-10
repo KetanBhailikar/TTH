ea16e709676fa476b44c2a39498464d7610e1830
Ketan Bhailikar <ketanbhailikar@gmail.com>
2022-10-10 21:31:09 +0530

    added scale variable to config

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ page_height = 2404                  # height of the page in pixels
 pipe_key_buffer = ""                # stores the characters after a pipe is encountered
 current_key_pipes_encountered = 0   # holds the number of pipes encountered in the current key
 current_page_number = 1             # holds the current page number
+scale = 1
 
 # Image variables set to None ( Initially )
 current_character_img = None        # holds the image of the current character
