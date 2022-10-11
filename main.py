34f3e7fae43edc7395064e8b2c5f160e28f1abae
Ketan Bhailikar <81752891+KetanBhailikar@users.noreply.github.com>
2022-10-11 22:41:55 +0530

    Added tracking of opening and closing symbols

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def main():
         # for all the words in a line
         for word in line.split():
             # initialise a blank word starting with a space if the key is not being read
-            if config.current_key_symbols_encountered == 0:
+            if config.current_key_symbols_encountered == 0 :
                 initialise_word()
 
             # loop for each character in a word
@@ -30,26 +30,26 @@ def main():
 
                 # if a start/end symbol is encountered, then key has started or closed
                 if character == config.key_start_symbol:
-                    config.current_key_symbols_encountered = 1
+                    config.current_key_symbols_encountered += 1
                 elif character == config.key_end_symbol:
-                    config.current_key_symbols_encountered = 2
+                    config.current_key_symbols_encountered -= 1
 
                 # if a key is being encountered then don't write it on paper,
-                # instead, put it in the pip key buffer for further analysis
-                if config.current_key_symbols_encountered > 0 and config.current_key_symbols_encountered < 3:
+                # instead, put it in the pipe key buffer for further analysis
+                if config.current_key_symbols_encountered > 0 or character == ">":
                     config.pipe_key_buffer += character
                 else:
                     write_character(character)                      # write the character 
 
                 # if the last pipe is encountered then the key has ended
-                if config.current_key_symbols_encountered == 2:
+                if config.current_key_symbols_encountered == 0 and config.pipe_key_buffer !="":
                     analyse_key()
                     config.current_key_symbols_encountered = 0          # reset the pipe counter
                     config.pipe_key_buffer = ""         # reset the pipe key buffer
 
             # if the key has multiple words in its text, then don't write those words to the line, instead,
             # add a space between the words in the pipe key buffer
-            if config.current_key_symbols_encountered > 0 and config.current_key_symbols_encountered < 3:
+            if config.current_key_symbols_encountered > 0 or character == ">":
                     config.pipe_key_buffer += " "
             else:
                 write_word()
